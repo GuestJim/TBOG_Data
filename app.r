503ebda9d2214769223228ebffb736e2402cd113
GuestJim <mlcforeigner@gmail.com>
2022-03-05 09:22:54 -0500

    Disable Intelligent Default
    ShinyApps uploads all of the data at the same time, so it is all the same time making the intelligent default useless. Other serving methods might prove better.

--- app.r
+++ app.r
@@ -8,6 +8,8 @@ FILES	=	list.files(path = "Data", pattern = "*.csv*")
 DATA$LOAD		=	FALSE	#used for tracking if data has been loaded automatically
 DATA$Default	=	FILES[which.max(file.mtime(paste0("Data/", FILES)))]
 #	with file.mtime, the modified timestamp is found and which.max will find the newest file in the list. In theory then, just updating the Data folder is enough, even though that's not an option with ShinyApps.io
+#		above not useful on ShinyApps.io because the modified times are when they all were uploaded
+DATA$Default	=	"Dead Rising 4.csv.bz2"
 GRAPH	=	new.env()
 #	rather than using super-assignment and pushing variables to Global, I'm putting them into this environment
 #	this keeps DATA within the Shiny environment too, so when Shiny ends, the data is apparently removed, which I'm good with
